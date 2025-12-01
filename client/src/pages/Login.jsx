e25a06bc65336b2cca92ebb6d4f56f363896938a
Pawan <154042595+This-is-pawan@users.noreply.github.com>
2025-12-01 21:51:10 +0530

    Update Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -15,7 +15,7 @@ const Login = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "http://localhost:3000/api/v1/auth/login",
+        "https://jobify-project-zxis.onrender.com/api/v1/auth/login",
         { name, password, email, location },
         { withCredentials: true }
       );
